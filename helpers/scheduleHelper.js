a08941abc0e0f8a5b90a47352841c1f3950915f5
rafaelrocha <rafaelmachadodarocha@gmail.com>
2020-10-16 15:07:52 -0300

    adding scheduleHelper

---
+++ helpers/scheduleHelper.js
@@ -0,0 +1,17 @@
+const DateHelper = require('./dateHelper');
+class ScheduleHelper {
+  static filterAvailableSchedules(scheduleRules) {
+    const schedules = [];
+    scheduleRules.forEach(function (rule) {
+      const formattedDate = DateHelper.formatDateToString(rule.day);
+      if (!schedules.some(element => element['day'] === formattedDate)) {
+        schedules.push({ day: formattedDate, intervals: [{ "start": rule.interval.start, "end": rule.interval.end }] });
+      } else {
+        const selectedElement = schedules.find(element => element['day'] === formattedDate);
+        selectedElement.intervals.push({ start: rule.interval.start, end: rule.interval.end });
+      }
+    });
+    return schedules;
+  }
+}
+module.exports = ScheduleHelper;
